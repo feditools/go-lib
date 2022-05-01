e8710d4a5dc9f8c6442a2723b44da626810a5db4
Tyr Mactire <tyr@pettingzoo.co>
2022-04-30 23:59:11 -0700

    text "ClientSecret"

--- language/text_c_test.go
+++ language/text_c_test.go
@@ -82,6 +82,44 @@ func TestLocalizer_TextClientID(t *testing.T) {
 	}
 }
 
+func TestLocalizer_TextClientSecret(t *testing.T) {
+	langMod, _ := New()
+
+	tables := []struct {
+		x language.Tag
+		c int
+		n string
+		l language.Tag
+	}{
+		{language.English, 1, "Client Secret", language.English},
+		{language.English, 2, "Client Secrets", language.English},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] Translating to %s", i, table.x)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.x.String())
+			if err != nil {
+				t.Errorf("[%d] can't get localizer for %s: %s", i, table.x, err.Error())
+				return
+			}
+
+			result := localizer.TextClientSecret(table.c)
+			if result.String() != table.n {
+				t.Errorf("[%d] got invalid translation for %s, got: %v, want: %v,", i, table.x, result.String(), table.n)
+			}
+			if result.Language() != table.l {
+				t.Errorf("[%d] got invalid language for %s, got: %v, want: %v,", i, table.x, result.Language(), table.l)
+			}
+		})
+	}
+}
+
 func TestLocalizer_TextCreate(t *testing.T) {
 	langMod, _ := New()
 
