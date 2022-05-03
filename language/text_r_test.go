6f46f10959994206e1a056a93c03b3363f531139
Tyr Mactire <tyr@pettingzoo.co>
2022-05-02 17:30:38 -0700

    text InvalidURI and Required (#6)

--- language/text_r_test.go
+++ language/text_r_test.go
@@ -6,6 +6,42 @@ import (
 	"testing"
 )
 
+func TestLocalizer_TextRequired(t *testing.T) {
+	langMod, _ := New()
+
+	tables := []struct {
+		x language.Tag
+		n string
+		l language.Tag
+	}{
+		{language.English, "Required", language.English},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] Translating to %s", i, table.x)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.x.String())
+			if err != nil {
+				t.Errorf("[%d] can't get localizer for %s: %s", i, table.x, err.Error())
+				return
+			}
+
+			result := localizer.TextRequired()
+			if result.String() != table.n {
+				t.Errorf("[%d] got invalid translation for %s, got: %v, want: %v,", i, table.x, result.String(), table.n)
+			}
+			if result.Language() != table.l {
+				t.Errorf("[%d] got invalid language for %s, got: %v, want: %v,", i, table.x, result.Language(), table.l)
+			}
+		})
+	}
+}
+
 func TestLocalizer_TextReturnURI(t *testing.T) {
 	langMod, _ := New()
 
