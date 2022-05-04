d6e43a5ddc4c136e2040a39dcccd466f82530372
Tyr Mactire <tyr@pettingzoo.co>
2022-05-03 22:10:48 -0700

    Common Template (#13)
    * common templates
    * tests

---
+++ template/template_test.go
@@ -0,0 +1,93 @@
+package template
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	templates, err := testNewTestTemplates()
+	if err != nil {
+		t.Errorf("unexpected error creating template: %s", err.Error())
+		return
+	}
+	if templates == nil {
+		t.Error("expected templates, got: nil")
+		return
+	}
+
+	result, err := testExecuteTemplate(templates, "test_test_func", "foo")
+	if err != nil {
+		t.Errorf("unexpected error creating template: %s", err.Error())
+		return
+	}
+	expected := "foo bar"
+	if result != expected {
+		t.Errorf("unexpected result\n\ngot:\n-------------\n%s\n\nwant:\n-------------\n%s\n", result, expected)
+		return
+	}
+}
+
+func addTestTemplates(templates *template.Template) error {
+	// open it
+	file, err := os.Open("../test/templates/test.gohtml")
+	if err != nil {
+		return err
+	}
+
+	// read it
+	tmplData, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	// It can now be parsed as a string.
+	_, err = templates.Parse(string(tmplData))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func testExecuteTemplate(templates *template.Template, name string, tmplVars interface{}) (string, error) {
+	b := new(bytes.Buffer)
+	err := templates.ExecuteTemplate(b, name, tmplVars)
+	if err != nil {
+		return "", err
+	}
+	return string(b.Bytes()), nil
+}
+
+func testNewTestTemplates() (*template.Template, error) {
+	testFuncs := template.FuncMap{
+		"testFunc": func(s string) string {
+			return fmt.Sprintf("%s bar", s)
+		},
+	}
+
+	tmpl, err := New(testFuncs)
+	if err != nil {
+		return nil, err
+	}
+
+	// add test templates
+	file, err := os.Open("../test/templates/test.gohtml")
+	if err != nil {
+		return nil, err
+	}
+	tmplData, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	_, err = tmpl.Parse(string(tmplData))
+	if err != nil {
+		return nil, err
+	}
+
+	return tmpl, nil
+}
