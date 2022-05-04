0b922fd5553503641ff39da34f7662fa29a8b8de
Tyr Mactire <tyr@pettingzoo.co>
2022-05-03 20:24:48 -0700

    some more testing

---
+++ template/template_functions_test.go
@@ -0,0 +1,66 @@
+package template
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTemplateFunctions(t *testing.T) {
+	templates, err := New(nil)
+	if err != nil {
+		t.Errorf("init: %s", err.Error())
+		return
+	}
+	err = addTestTemplates(templates)
+	if err != nil {
+		t.Errorf("adding tests: %s", err.Error())
+		return
+	}
+
+	tables := []struct {
+		templateName string
+		templateVars interface{}
+		output       string
+	}{
+		{
+			"test_dec",
+			481,
+			"480",
+		},
+		{
+			"test_html_safe",
+			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;'[]\\`~!@#$%^&*()_+{}|:\"<>?",
+			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;'[]\\`~!@#$%^&*()_+{}|:\"<>?",
+		},
+		{
+			"test_inc",
+			48,
+			"49",
+		},
+		{
+			"test_passthrough",
+			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;'[]\\`~!@#$%^&*()_+{}|:\"<>?",
+			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;&#39;[]\\`~!@#$%^&amp;*()_&#43;{}|:&#34;&lt;&gt;?",
+		},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] Running making pagination", i)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			result, err := testExecuteTemplate(templates, table.templateName, table.templateVars)
+			if err != nil {
+				t.Errorf("unexpected error creating template: %s", err.Error())
+				return
+			}
+			if result != table.output {
+				t.Errorf("[%d] unexpected result\n\ngot:\n-------------\n%s\n\nwant:\n-------------\n%s\n", i, result, table.output)
+				return
+			}
+		})
+	}
+}
