9726a349eb017f06ffe07368ad5f93feb862e5c6
Tyr Mactire <tyr@pettingzoo.co>
2022-05-03 22:31:13 -0700

    check if yaml is empty

---
+++ language/util_test.go
@@ -0,0 +1,33 @@
+package language
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsEmptyYaml(t *testing.T) {
+	tables := []struct {
+		input  string
+		output bool
+	}{
+		{"", true},
+		{"---", true},
+		{"---\n", true},
+		{"---\nvalid: yaml", false},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] running isEmptyYaml", i)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			result := isEmptyYaml([]byte(table.input))
+			if result != table.output {
+				t.Errorf("[%d] got invalid response, got: %v, want: %v,", i, result, table.output)
+			}
+		})
+	}
+}
