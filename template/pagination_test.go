2466b179c7e8f114173848fb890cd82e2e01227d
Tyr Mactire <tyr@pettingzoo.co>
2022-05-03 21:06:50 -0700

    pagination first page

--- template/pagination_test.go
+++ template/pagination_test.go
@@ -7,19 +7,59 @@ import (
 
 const testPaginationResult1 = `<nav>
   <ul class="pagination">
+    <li class="page-item disabled">
+      <span class="page-link" aria-label="Previous">
+        <span aria-hidden="true"><i class="fas fa-caret-left"></i></span>
+      </span>
+    </li>
+    <li class="page-item active" aria-current="page">
+      <span class="page-link" aria-label="1">
+        1 <span class="sr-only">(current)</span>
+      </span>
+    </li>
     <li class="page-item">
-      <a class="page-link" href="/test1?page=16">
-        <span aria-hidden="true"><i class="fas fa-caret-left"></i></span> 
+      <a class="page-link" href="/test1?page=2">
+        2
       </a>
     </li>
     <li class="page-item">
-      <a class="page-link" href="/test1?page=15">
-        15 
+      <a class="page-link" href="/test1?page=3">
+        3
       </a>
     </li>
     <li class="page-item">
-      <a class="page-link" href="/test1?page=16">
-        16 
+      <a class="page-link" href="/test1?page=4">
+        4
+      </a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="/test1?page=5">
+        5
+      </a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="/test1?page=2">
+        <span aria-hidden="true"><i class="fas fa-caret-right"></i></span>
+      </a>
+    </li>
+  </ul>
+</nav>`
+
+const testPaginationResult2 = `<nav>
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="/test2?page=16">
+        <span aria-hidden="true"><i class="fas fa-caret-left"></i></span>
+      </a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="/test2?page=15">
+        15
+      </a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="/test2?page=16">
+        16
       </a>
     </li>
     <li class="page-item active" aria-current="page">
@@ -28,18 +68,18 @@ const testPaginationResult1 = `<nav>
       </span>
     </li>
     <li class="page-item">
-      <a class="page-link" href="/test1?page=18">
-        18 
+      <a class="page-link" href="/test2?page=18">
+        18
       </a>
     </li>
     <li class="page-item">
-      <a class="page-link" href="/test1?page=19">
-        19 
+      <a class="page-link" href="/test2?page=19">
+        19
       </a>
     </li>
     <li class="page-item">
-      <a class="page-link" href="/test1?page=18">
-        <span aria-hidden="true"><i class="fas fa-caret-right"></i></span> 
+      <a class="page-link" href="/test2?page=18">
+        <span aria-hidden="true"><i class="fas fa-caret-right"></i></span>
       </a>
     </li>
   </ul>
@@ -67,9 +107,19 @@ func TestMakePagination(t *testing.T) {
 				DisplayCount:  20,
 				HRef:          "/test1",
 				MaxPagination: 5,
-				Page:          17,
+				Page:          1,
 			},
 			testPaginationResult1,
+		},
+		{
+			&PaginationConfig{
+				Count:         1000,
+				DisplayCount:  20,
+				HRef:          "/test2",
+				MaxPagination: 5,
+				Page:          17,
+			},
+			testPaginationResult2,
 		},
 	}
 
