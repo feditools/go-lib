5541145226c3eb475f9483c2ca9e176f8aa330ae
Tyr Mactire <tyr@pettingzoo.co>
2022-05-04 12:37:02 -0700

    text Fediverse and Instance

--- language/text_i_test.go
+++ language/text_i_test.go
@@ -6,6 +6,44 @@ import (
 	"testing"
 )
 
+func TestLocalizer_TextInstance(t *testing.T) {
+	langMod, _ := New()
+
+	tables := []struct {
+		x language.Tag
+		c int
+		n string
+		l language.Tag
+	}{
+		{language.English, 1, "Instance", language.English},
+		{language.English, 2, "Instances", language.English},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] Translating to %s", i, table.x)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.x.String())
+			if err != nil {
+				t.Errorf("[%d] can't get localizer for %s: %s", i, table.x, err.Error())
+				return
+			}
+
+			result := localizer.TextInstance(table.c)
+			if result.String() != table.n {
+				t.Errorf("[%d] got invalid translation for %s, got: %v, want: %v,", i, table.x, result.String(), table.n)
+			}
+			if result.Language() != table.l {
+				t.Errorf("[%d] got invalid language for %s, got: %v, want: %v,", i, table.x, result.Language(), table.l)
+			}
+		})
+	}
+}
+
 func TestLocalizer_TextInvalidURI(t *testing.T) {
 	langMod, _ := New()
 
