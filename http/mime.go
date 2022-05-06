3fd363057bf106495481d5f9e5849385ae018c74
Tyr Mactire <tyr@pettingzoo.co>
2022-05-05 19:15:01 -0700

    add mimes

---
+++ http/mime.go
@@ -0,0 +1,10 @@
+package http
+
+const (
+	MimeAll               = `*/*`
+	MimeAppJRDJSON        = `application/jrd+json`
+	MimeAppJSON           = `application/json`
+	MimeAppActivityJSON   = `application/activity+json`
+	MimeAppActivityLDJSON = `application/ld+json; profile="https://www.w3.org/ns/activitystreams"`
+	MimeTextHTML          = `text/html`
+)
