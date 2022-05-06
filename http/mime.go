b6388624d29f3e584b6877f9b369fbca3ef75ca6
Tyr Mactire <tyr@pettingzoo.co>
2022-05-06 13:46:23 -0700

    label mimes

--- http/mime.go
+++ http/mime.go
@@ -1,10 +1,16 @@
 package http
 
 const (
-	MimeAll               = `*/*`
-	MimeAppJRDJSON        = `application/jrd+json`
-	MimeAppJSON           = `application/json`
-	MimeAppActivityJSON   = `application/activity+json`
+	// MimeAll matches any mime type
+	MimeAll = `*/*`
+	// MimeAppJRDJSON represents a JSON Resource Descriptor type
+	MimeAppJRDJSON = `application/jrd+json`
+	// MimeAppJSON represents a JavaScript Object Notation type
+	MimeAppJSON = `application/json`
+	// MimeAppActivityJSON represents a JSON activity pub action type
+	MimeAppActivityJSON = `application/activity+json`
+	// MimeAppActivityLDJSON represents JSON-based Linked Data for activity streams type
 	MimeAppActivityLDJSON = `application/ld+json; profile="https://www.w3.org/ns/activitystreams"`
-	MimeTextHTML          = `text/html`
+	// MimeTextHTML represents a html type
+	MimeTextHTML = `text/html`
 )
