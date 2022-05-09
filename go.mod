09db707057e97ccd64eebcc8e7b600a231c5d7af
Tyr Mactire <tyr@pettingzoo.co>
2022-05-09 15:24:43 -0700

    credential

--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ require (
 	github.com/nicksnyder/go-i18n/v2 v2.2.0
 	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/text v0.3.7
+	google.golang.org/grpc v1.46.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
---
+++ grpc/credential.go
@@ -0,0 +1,38 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc/credentials"
+)
+
+// Credential contains feditools grpc token
+type Credential struct {
+	token string
+}
+
+// NewCredential creates a new feditools grpc token
+func NewCredential(token string) *Credential {
+	return &Credential{
+		token: token,
+	}
+}
+
+// GetRequestMetadata add the token to the request
+func (c *Credential) GetRequestMetadata(ctx context.Context, _ ...string) (map[string]string, error) {
+	ri, _ := credentials.RequestInfoFromContext(ctx)
+	fmt.Printf("request info: %+v\n", ri)
+
+	if err := credentials.CheckSecurityLevel(ri.AuthInfo, credentials.PrivacyAndIntegrity); err != nil {
+		return nil, fmt.Errorf("security %s level too low: %s", ri.AuthInfo, err.Error())
+	}
+
+	return map[string]string{
+		"authorization": c.token,
+	}, nil
+}
+
+// RequireTransportSecurity returns false since transport security is not required
+func (c *Credential) RequireTransportSecurity() bool {
+	return false
+}
