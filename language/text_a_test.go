981ea4e78d98a6f07e2ad3a1121b71b77a2255cd
Tyr Mactire <tyr@pettingzoo.co>
2022-05-10 23:40:50 -0700

    add TextApplicationToken

--- language/text_a_test.go
+++ language/text_a_test.go
@@ -84,3 +84,42 @@ func TestLocalizer_TextAddOauth20Client(t *testing.T) {
 		})
 	}
 }
+
+func TestLocalizer_TextApplicationToken(t *testing.T) {
+	t.Parallel()
+
+	tables := []testTextTable{
+		{
+			inputLang:    language.English,
+			inputCount:   1,
+			outputString: "Application Token",
+			outputLang:   language.English,
+		},
+		{
+			inputLang:    language.English,
+			inputCount:   2,
+			outputString: "Application Tokens",
+			outputLang:   language.English,
+		},
+	}
+
+	langMod, _ := New()
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf(testTranslatedTo, i, table.inputLang)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.inputLang.String())
+			if err != nil {
+				t.Errorf(testCantGetLocalizer, i, table.inputLang, err.Error())
+
+				return
+			}
+
+			testTextWithCount(t, i, localizer.TextApplicationToken, table)
+		})
+	}
+}
