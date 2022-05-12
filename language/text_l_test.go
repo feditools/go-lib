9561371d1bbea5568c5283693279a76bae43e0cb
Tyr Mactire <tyr@pettingzoo.co>
2022-05-12 01:22:13 -0700

    add TextList

--- language/text_l_test.go
+++ language/text_l_test.go
@@ -39,6 +39,45 @@ func TestLocalizer_TextLogin(t *testing.T) {
 	}
 }
 
+func TestLocalizer_TextList(t *testing.T) {
+	t.Parallel()
+
+	tables := []testTextTable{
+		{
+			inputLang:    language.English,
+			inputCount:   1,
+			outputString: "List",
+			outputLang:   language.English,
+		},
+		{
+			inputLang:    language.English,
+			inputCount:   2,
+			outputString: "Lists",
+			outputLang:   language.English,
+		},
+	}
+
+	langMod, _ := New()
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf(testTranslatedTo, i, table.inputLang)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.inputLang.String())
+			if err != nil {
+				t.Errorf(testCantGetLocalizer, i, table.inputLang, err.Error())
+
+				return
+			}
+
+			testTextWithCount(t, i, localizer.TextList, table)
+		})
+	}
+}
+
 func TestLocalizer_TextLookGood(t *testing.T) {
 	t.Parallel()
 
