44f0bf1b8d4392d1a2f4caa84ac275349cbd3caa
Tyr Mactire <tyr@pettingzoo.co>
2022-05-18 11:10:21 -0700

    add HeaderCacheControl

--- http/header.go
+++ http/header.go
@@ -1,6 +1,8 @@
 package http
 
 const (
+	// HeaderCacheControl is the key for the cache control header.
+	HeaderCacheControl = "Cache-Control"
 	// HeaderContentType is the key for the content type header.
 	HeaderContentType = "Content-Type"
 )
