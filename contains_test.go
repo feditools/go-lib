e327213fea0be74eb43d21d96ec17a6337b38cb4
Tyr Mactire <tyr@pettingzoo.co>
2022-05-25 15:38:12 -0700

    contains

---
+++ contains_test.go
@@ -0,0 +1,74 @@
+package lib
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestContainsString(t *testing.T) {
+	stack := []string{
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+	}
+
+	tables := []struct {
+		x string
+		n bool
+	}{
+		{"one", true},
+		{"four", true},
+		{"foo", false},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] Checking stack for %s", i, table.x)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			result := ContainsString(stack, table.x)
+			if result != table.n {
+				t.Errorf("Sum was incorrect, got: %v, want: %v.", result, table.n)
+			}
+		})
+	}
+}
+
+func TestContainsOneOfStrings(t *testing.T) {
+	stack := []string{
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+	}
+
+	tables := []struct {
+		x []string
+		n bool
+	}{
+		{[]string{"one", "two", "three"}, true},
+		{[]string{"foo", "five", "bar"}, true},
+		{[]string{"foo", "bar", "fizz"}, false},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] Checking stack for %s", i, table.x)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			result := ContainsOneOfStrings(stack, table.x)
+			if result != table.n {
+				t.Errorf("Sum was incorrect, got: %v, want: %v.", result, table.n)
+			}
+		})
+	}
+}
