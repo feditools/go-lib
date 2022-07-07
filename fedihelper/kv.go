ec4f3eb71922490a3fce67e896976cfe472a9549
Tyr Mactire <tyr@pettingzoo.co>
2022-07-06 20:35:26 -0700

    instance oauth

--- fedihelper/kv.go
+++ fedihelper/kv.go
@@ -8,9 +8,15 @@ import (
 type KV interface {
 	// access token
 
-	DeleteAccessToken(ctx context.Context, userID int64) (err error)
-	GetAccessToken(ctx context.Context, userID int64) (accessToken string, err error)
-	SetAccessToken(ctx context.Context, userID int64, accessToken string) (err error)
+	DeleteAccessToken(ctx context.Context, accountID int64) (err error)
+	GetAccessToken(ctx context.Context, accountID int64) (accessToken string, err error)
+	SetAccessToken(ctx context.Context, accountID int64, accessToken string) (err error)
+
+	// instance oauth
+
+	DeleteInstanceOAuth(ctx context.Context, instanceID int64) (err error)
+	GetInstanceOAuth(ctx context.Context, instanceID int64) (clientID string, clientSecret string, err error)
+	SetInstanceOAuth(ctx context.Context, instanceID int64, clientID string, clientSecret string) (err error)
 
 	// federated instance node info
 
