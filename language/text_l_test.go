41e8276d3b31fa19a7e2b9981b8ec09f8cc36006
Tyr Mactire <tyr@pettingzoo.co>
2022-07-18 16:21:17 -0700

    TextLogout

--- language/text_l_test.go
+++ language/text_l_test.go
@@ -39,6 +39,38 @@ func TestLocalizer_TextLogin(t *testing.T) {
 	}
 }
 
+func TestLocalizer_TextLogout(t *testing.T) {
+	t.Parallel()
+
+	tables := []testTextTable{
+		{
+			inputLang:    language.English,
+			outputString: "Logout",
+			outputLang:   language.English,
+		},
+	}
+
+	langMod, _ := New()
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf(testTranslatedTo, i, table.inputLang)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.inputLang.String())
+			if err != nil {
+				t.Errorf(testCantGetLocalizer, i, table.inputLang, err.Error())
+
+				return
+			}
+
+			testText(t, i, localizer.TextLogout, table)
+		})
+	}
+}
+
 func TestLocalizer_TextList(t *testing.T) {
 	t.Parallel()
 
