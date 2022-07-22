75cd86e95eeac83b0d934801c2903628bbf3e307
Tyr Mactire <tyr@pettingzoo.co>
2022-07-21 20:58:23 -0700

    TextClose

--- language/text_c_test.go
+++ language/text_c_test.go
@@ -124,6 +124,38 @@ func TestLocalizer_TextClientSecret(t *testing.T) {
 	}
 }
 
+func TestLocalizer_TextClose(t *testing.T) {
+	t.Parallel()
+
+	tables := []testTextTable{
+		{
+			inputLang:    language.English,
+			outputString: "Close",
+			outputLang:   language.English,
+		},
+	}
+
+	langMod, _ := New()
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf(testTranslatedTo, i, table.inputLang)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.inputLang.String())
+			if err != nil {
+				t.Errorf(testCantGetLocalizer, i, table.inputLang, err.Error())
+
+				return
+			}
+
+			testText(t, i, localizer.TextClose, table)
+		})
+	}
+}
+
 func TestLocalizer_TextCreate(t *testing.T) {
 	t.Parallel()
 
