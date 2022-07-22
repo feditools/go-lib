50bb06bf55d809fa21de425ab32b3ceeb4683220
Tyr Mactire <tyr@pettingzoo.co>
2022-07-21 20:41:19 -0700

    block text

--- language/text_b_test.go
+++ language/text_b_test.go
@@ -45,6 +45,84 @@ func TestLocalizer_TextBlock(t *testing.T) {
 	}
 }
 
+func TestLocalizer_TextBlockExists(t *testing.T) {
+	t.Parallel()
+
+	tables := []testTextTable{
+		{
+			inputLang:    language.English,
+			inputStrings: []string{"example.com"},
+			outputString: "Block for domain example.com already exists.",
+			outputLang:   language.English,
+		},
+		{
+			inputLang:    language.English,
+			inputStrings: []string{"example2.com"},
+			outputString: "Block for domain example2.com already exists.",
+			outputLang:   language.English,
+		},
+	}
+
+	langMod, _ := New()
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf(testTranslatedTo, i, table.inputLang)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.inputLang.String())
+			if err != nil {
+				t.Errorf(testCantGetLocalizer, i, table.inputLang, err.Error())
+
+				return
+			}
+
+			testTextWith1String(t, i, localizer.TextBlockExists, table)
+		})
+	}
+}
+
+func TestLocalizer_TextBlockSubdomain(t *testing.T) {
+	t.Parallel()
+
+	tables := []testTextTable{
+		{
+			inputLang:    language.English,
+			inputCount:   1,
+			outputString: "Block Subdomain",
+			outputLang:   language.English,
+		},
+		{
+			inputLang:    language.English,
+			inputCount:   2,
+			outputString: "Block Subdomains",
+			outputLang:   language.English,
+		},
+	}
+
+	langMod, _ := New()
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf(testTranslatedTo, i, table.inputLang)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.inputLang.String())
+			if err != nil {
+				t.Errorf(testCantGetLocalizer, i, table.inputLang, err.Error())
+
+				return
+			}
+
+			testTextWithCount(t, i, localizer.TextBlockSubdomain, table)
+		})
+	}
+}
+
 func TestLocalizer_TextBlockedInstance(t *testing.T) {
 	t.Parallel()
 
--- language/text_d_test.go
+++ language/text_d_test.go
@@ -116,3 +116,42 @@ func TestLocalizer_TextDescription(t *testing.T) {
 		})
 	}
 }
+
+func TestLocalizer_TextDomain(t *testing.T) {
+	t.Parallel()
+
+	tables := []testTextTable{
+		{
+			inputLang:    language.English,
+			inputCount:   1,
+			outputString: "Domain",
+			outputLang:   language.English,
+		},
+		{
+			inputLang:    language.English,
+			inputCount:   2,
+			outputString: "Domains",
+			outputLang:   language.English,
+		},
+	}
+
+	langMod, _ := New()
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf(testTranslatedTo, i, table.inputLang)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.inputLang.String())
+			if err != nil {
+				t.Errorf(testCantGetLocalizer, i, table.inputLang, err.Error())
+
+				return
+			}
+
+			testTextWithCount(t, i, localizer.TextDomain, table)
+		})
+	}
+}
