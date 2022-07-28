428faeddd76435fd1d7a564b547723273eca0a83
Tyr Mactire <tyr@pettingzoo.co>
2022-07-28 13:37:26 -0700

    TextErrorDatabase

--- language/text_e_test.go
+++ language/text_e_test.go
@@ -44,3 +44,35 @@ func TestLocalizer_TextEditBlock(t *testing.T) {
 		})
 	}
 }
+
+func TestLocalizer_TextErrorDatabase(t *testing.T) {
+	t.Parallel()
+
+	tables := []testTextTable{
+		{
+			inputLang:    language.English,
+			outputString: "database error",
+			outputLang:   language.English,
+		},
+	}
+
+	langMod, _ := New()
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf(testTranslatedTo, i, table.inputLang)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.inputLang.String())
+			if err != nil {
+				t.Errorf(testCantGetLocalizer, i, table.inputLang, err.Error())
+
+				return
+			}
+
+			testText(t, i, localizer.TextErrorDatabase, table)
+		})
+	}
+}
