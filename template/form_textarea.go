130660f7fa5815241d6152230233abb3aece243d
Tyr Mactire <tyr@pettingzoo.co>
2022-07-29 18:20:09 -0700

    form textarea Label

--- template/form_textarea.go
+++ template/form_textarea.go
@@ -1,14 +1,11 @@
 package template
 
-import liblanguage "github.com/feditools/go-lib/language"
-
 // FormTextarea is a templated form textarea.
 type FormTextarea struct {
 	ID         string
 	Name       string
 	Rows       int
-	Label      *liblanguage.LocalizedString
-	LabelClass string
+	Label      *FormLabel
 	Value      string
 	Disabled   bool
 	Required   bool
